b537e5ae62d975e47decbd75fde95d8a42d0eec4
Tae Young Kim <taeyok@microsoft.com>
2025-11-05 11:49:05 +0900

    feat: 5단계 - 메모 앱 뷰 및 폼 구현 완료
    - MemoForm 구현 (제목/내용 위젯 커스터마이징, 입력값 검증)
    - MemoListView 구현 (페이지네이션, 사용자별 메모 필터링)
    - MemoDetailView 구현 (작성자 권한 검증)
    - MemoCreateView 구현 (작성자 자동 설정)
    - MemoUpdateView 구현 (작성자만 수정 가능)
    - MemoDeleteView 구현 (작성자만 삭제 가능)
    - RESTful URL 패턴 정의
    - LoginRequiredMixin, UserPassesTestMixin 활용한 권한 검증
    
    Closes #26

--- Memo/apps/memos/urls.py
+++ Memo/apps/memos/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
+from . import views
 
-# 임시 URL 패턴 (4단계에서 구현 예정)
-urlpatterns = []
+urlpatterns = [
+    path('', views.MemoListView.as_view(), name='memo_list'),
+    path('create/', views.MemoCreateView.as_view(), name='memo_create'),
+    path('<int:pk>/', views.MemoDetailView.as_view(), name='memo_detail'),
+    path('<int:pk>/update/', views.MemoUpdateView.as_view(), name='memo_update'),
+    path('<int:pk>/delete/', views.MemoDeleteView.as_view(), name='memo_delete'),
+]
--- Memo/apps/memos/views.py
+++ Memo/apps/memos/views.py
@@ -1,3 +1,81 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib import messages
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView
+)
+from django.urls import reverse_lazy
+from django.core.paginator import Paginator
+from .models import Memo
+from .forms import MemoForm
 
-# Create your views here.
+
+class MemoListView(LoginRequiredMixin, ListView):
+    """메모 목록 뷰"""
+    model = Memo
+    template_name = 'memos/memo_list.html'
+    context_object_name = 'memos'
+    paginate_by = 10
+
+    def get_queryset(self):
+        """현재 사용자의 메모만 조회"""
+        return Memo.objects.filter(author=self.request.user).select_related('author')
+
+
+class MemoDetailView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
+    """메모 상세 뷰"""
+    model = Memo
+    template_name = 'memos/memo_detail.html'
+    context_object_name = 'memo'
+
+    def test_func(self):
+        """작성자만 접근 가능"""
+        memo = self.get_object()
+        return self.request.user == memo.author
+
+
+class MemoCreateView(LoginRequiredMixin, CreateView):
+    """메모 작성 뷰"""
+    model = Memo
+    form_class = MemoForm
+    template_name = 'memos/memo_form.html'
+
+    def form_valid(self, form):
+        """작성자 자동 설정"""
+        form.instance.author = self.request.user
+        messages.success(self.request, '메모가 성공적으로 작성되었습니다!')
+        return super().form_valid(form)
+
+
+class MemoUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """메모 수정 뷰"""
+    model = Memo
+    form_class = MemoForm
+    template_name = 'memos/memo_form.html'
+
+    def test_func(self):
+        """작성자만 수정 가능"""
+        memo = self.get_object()
+        return self.request.user == memo.author
+
+    def form_valid(self, form):
+        messages.success(self.request, '메모가 성공적으로 수정되었습니다!')
+        return super().form_valid(form)
+
+
+class MemoDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """메모 삭제 뷰"""
+    model = Memo
+    template_name = 'memos/memo_confirm_delete.html'
+    success_url = reverse_lazy('memo_list')
+
+    def test_func(self):
+        """작성자만 삭제 가능"""
+        memo = self.get_object()
+        return self.request.user == memo.author
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(request, '메모가 성공적으로 삭제되었습니다!')
+        return super().delete(request, *args, **kwargs)
+
