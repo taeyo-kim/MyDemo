d1ec0f2950d13e209c8aba82ba778a3d57940eb5
Tae Young Kim <taeyok@microsoft.com>
2025-11-11 15:27:52 +0900

    feat: 8단계 완료 - 관리자 인터페이스 설정 및 프로젝트 완료
    - PostAdmin 클래스 생성 및 커스터마이징
    - list_display, list_filter, search_fields 설정
    - readonly_fields, date_hierarchy 추가
    - fieldsets로 관리자 폼 구조화
    - select_related로 쿼리 최적화
    - README.md 작성 (설치, 실행, 사용법)
    - 슈퍼유저 생성 가이드 추가
    
    Resolves #46

--- Blog/apps/users/admin.py
+++ Blog/apps/users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+# Django 기본 User 모델 사용
+# 필요시 커스터마이징 가능
+
