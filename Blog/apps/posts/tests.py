37cadf21d47b13ab9e4b12c916cc57fc436706f8
Tae Young Kim <taeyok@microsoft.com>
2026-02-19 15:51:12 +0900

    feat: Phase 9 - 테스트 작성
    - apps/posts/tests.py: Post 관련 테스트
      * PostModelTest: 모델 생성, __str__, 정렬 테스트
      * PostListViewTest: 목록 조회, 정렬 기능 테스트
      * PostDetailViewTest: 상세 조회, 조회수 증가 테스트
      * PostCreateViewTest: 로그인 필수, 글 작성 테스트
      * PostUpdateViewTest: 작성자 권한, 글 수정 테스트
      * PostDeleteViewTest: 작성자 권한, 글 삭제 테스트
    - apps/users/tests.py: 인증 관련 테스트
      * SignUpViewTest: 회원가입 성공/실패 테스트
      * LoginViewTest: 로그인 성공/실패 테스트
      * LogoutViewTest: 로그아웃 테스트
    - 총 19개 테스트 작성
    
    Issue: #72

--- Blog/apps/posts/tests.py
+++ Blog/apps/posts/tests.py
@@ -1,3 +1,206 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.urls import reverse
+from .models import Post
 
-# Create your tests here.
+
+class PostModelTest(TestCase):
+    """Post 모델 테스트"""
+    
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpass123'
+        )
+        self.post = Post.objects.create(
+            title='테스트 제목',
+            content='테스트 내용입니다.',
+            author=self.user
+        )
+    
+    def test_post_creation(self):
+        """글 생성 테스트"""
+        self.assertEqual(self.post.title, '테스트 제목')
+        self.assertEqual(self.post.content, '테스트 내용입니다.')
+        self.assertEqual(self.post.author, self.user)
+        self.assertEqual(self.post.views, 0)
+    
+    def test_post_str(self):
+        """__str__ 메서드 테스트"""
+        self.assertEqual(str(self.post), '테스트 제목')
+    
+    def test_post_ordering(self):
+        """정렬 테스트 (최신순)"""
+        post2 = Post.objects.create(
+            title='두 번째 글',
+            content='두 번째 내용',
+            author=self.user
+        )
+        posts = Post.objects.all()
+        self.assertEqual(posts[0], post2)  # 최신 글이 먼저
+        self.assertEqual(posts[1], self.post)
+
+
+class PostListViewTest(TestCase):
+    """PostListView 테스트"""
+    
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpass123'
+        )
+    
+    def test_post_list_view(self):
+        """글 목록 조회 테스트"""
+        response = self.client.get(reverse('posts:post_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'posts/post_list.html')
+    
+    def test_post_list_sorting_by_views(self):
+        """조회수순 정렬 테스트"""
+        post1 = Post.objects.create(
+            title='첫 번째 글',
+            content='내용1',
+            author=self.user,
+            views=10
+        )
+        post2 = Post.objects.create(
+            title='두 번째 글',
+            content='내용2',
+            author=self.user,
+            views=20
+        )
+        response = self.client.get(reverse('posts:post_list') + '?sort=views')
+        self.assertEqual(response.context['posts'][0], post2)
+
+
+class PostDetailViewTest(TestCase):
+    """PostDetailView 테스트"""
+    
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpass123'
+        )
+        self.post = Post.objects.create(
+            title='테스트 글',
+            content='테스트 내용',
+            author=self.user
+        )
+    
+    def test_post_detail_view(self):
+        """글 상세 조회 테스트"""
+        response = self.client.get(
+            reverse('posts:post_detail', kwargs={'pk': self.post.pk})
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '테스트 글')
+    
+    def test_post_views_increment(self):
+        """조회수 증가 테스트"""
+        initial_views = self.post.views
+        self.client.get(reverse('posts:post_detail', kwargs={'pk': self.post.pk}))
+        self.post.refresh_from_db()
+        self.assertEqual(self.post.views, initial_views + 1)
+
+
+class PostCreateViewTest(TestCase):
+    """PostCreateView 테스트"""
+    
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpass123'
+        )
+    
+    def test_post_create_view_requires_login(self):
+        """비로그인 사용자의 글 작성 시도 테스트"""
+        response = self.client.get(reverse('posts:post_create'))
+        self.assertEqual(response.status_code, 302)  # 로그인 페이지로 리다이렉트
+        self.assertIn('/users/login/', response.url)
+    
+    def test_post_create_view_with_login(self):
+        """로그인한 사용자의 글 작성 테스트"""
+        self.client.login(username='testuser', password='testpass123')
+        response = self.client.post(
+            reverse('posts:post_create'),
+            {
+                'title': '새 글 제목',
+                'content': '새 글 내용'
+            }
+        )
+        self.assertEqual(response.status_code, 302)  # 성공 후 리다이렉트
+        self.assertEqual(Post.objects.count(), 1)
+        post = Post.objects.first()
+        self.assertEqual(post.title, '새 글 제목')
+        self.assertEqual(post.author, self.user)
+
+
+class PostUpdateViewTest(TestCase):
+    """PostUpdateView 테스트"""
+    
+    def setUp(self):
+        self.client = Client()
+        self.user1 = User.objects.create_user(
+            username='user1',
+            password='pass123'
+        )
+        self.user2 = User.objects.create_user(
+            username='user2',
+            password='pass123'
+        )
+        self.post = Post.objects.create(
+            title='원본 제목',
+            content='원본 내용',
+            author=self.user1
+        )
+    
+    def test_post_update_by_author(self):
+        """작성자의 글 수정 테스트"""
+        self.client.login(username='user1', password='pass123')
+        response = self.client.post(
+            reverse('posts:post_update', kwargs={'pk': self.post.pk}),
+            {
+                'title': '수정된 제목',
+                'content': '수정된 내용'
+            }
+        )
+        self.post.refresh_from_db()
+        self.assertEqual(self.post.title, '수정된 제목')
+    
+    def test_post_update_by_non_author(self):
+        """작성자가 아닌 사용자의 글 수정 시도 테스트"""
+        self.client.login(username='user2', password='pass123')
+        response = self.client.get(
+            reverse('posts:post_update', kwargs={'pk': self.post.pk})
+        )
+        self.assertEqual(response.status_code, 403)  # Forbidden
+
+
+class PostDeleteViewTest(TestCase):
+    """PostDeleteView 테스트"""
+    
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpass123'
+        )
+        self.post = Post.objects.create(
+            title='삭제할 글',
+            content='삭제할 내용',
+            author=self.user
+        )
+    
+    def test_post_delete_by_author(self):
+        """작성자의 글 삭제 테스트"""
+        self.client.login(username='testuser', password='testpass123')
+        response = self.client.post(
+            reverse('posts:post_delete', kwargs={'pk': self.post.pk})
+        )
+        self.assertEqual(response.status_code, 302)  # 성공 후 리다이렉트
+        self.assertEqual(Post.objects.count(), 0)
+
--- Blog/apps/users/tests.py
+++ Blog/apps/users/tests.py
@@ -1,3 +1,106 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+
+class SignUpViewTest(TestCase):
+    """회원가입 테스트"""
+    
+    def setUp(self):
+        self.client = Client()
+    
+    def test_signup_view_get(self):
+        """회원가입 페이지 접근 테스트"""
+        response = self.client.get(reverse('users:signup'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/signup.html')
+    
+    def test_signup_success(self):
+        """회원가입 성공 테스트"""
+        response = self.client.post(
+            reverse('users:signup'),
+            {
+                'username': 'newuser',
+                'email': 'newuser@example.com',
+                'password1': 'testpass123!@#',
+                'password2': 'testpass123!@#'
+            }
+        )
+        self.assertEqual(response.status_code, 302)  # 로그인 페이지로 리다이렉트
+        self.assertTrue(User.objects.filter(username='newuser').exists())
+    
+    def test_signup_invalid_password(self):
+        """잘못된 비밀번호로 회원가입 시도 테스트"""
+        response = self.client.post(
+            reverse('users:signup'),
+            {
+                'username': 'newuser',
+                'email': 'newuser@example.com',
+                'password1': 'testpass123!@#',
+                'password2': 'differentpass'
+            }
+        )
+        self.assertEqual(response.status_code, 200)  # 폼 에러로 같은 페이지
+        self.assertFalse(User.objects.filter(username='newuser').exists())
+
+
+class LoginViewTest(TestCase):
+    """로그인 테스트"""
+    
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpass123'
+        )
+    
+    def test_login_view_get(self):
+        """로그인 페이지 접근 테스트"""
+        response = self.client.get(reverse('users:login'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/login.html')
+    
+    def test_login_success(self):
+        """로그인 성공 테스트"""
+        response = self.client.post(
+            reverse('users:login'),
+            {
+                'username': 'testuser',
+                'password': 'testpass123'
+            }
+        )
+        self.assertEqual(response.status_code, 302)  # 홈페이지로 리다이렉트
+        self.assertTrue(response.wsgi_request.user.is_authenticated)
+    
+    def test_login_invalid_credentials(self):
+        """잘못된 인증 정보로 로그인 시도 테스트"""
+        response = self.client.post(
+            reverse('users:login'),
+            {
+                'username': 'testuser',
+                'password': 'wrongpassword'
+            }
+        )
+        self.assertEqual(response.status_code, 200)  # 폼 에러로 같은 페이지
+        self.assertFalse(response.wsgi_request.user.is_authenticated)
+
+
+class LogoutViewTest(TestCase):
+    """로그아웃 테스트"""
+    
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpass123'
+        )
+    
+    def test_logout(self):
+        """로그아웃 테스트"""
+        self.client.login(username='testuser', password='testpass123')
+        response = self.client.get(reverse('users:logout'))
+        self.assertEqual(response.status_code, 302)  # 홈페이지로 리다이렉트
+        # 로그아웃 후 인증 상태 확인
+        response = self.client.get(reverse('posts:post_list'))
+        self.assertFalse(response.wsgi_request.user.is_authenticated)
+
