b042f101e8e89e5e627cf65b17e57f521fc885f3
Tae Young Kim <taeyok@microsoft.com>
2026-02-19 15:39:31 +0900

    feat: Phase 3 - 데이터 모델 구현
    - Post 모델 정의:
      * title (제목)
      * content (내용)
      * author (작성자, User ForeignKey)
      * created_at (작성일, auto_now_add)
      * updated_at (수정일, auto_now)
      * views (조회수, default=0)
      * Meta: ordering by -created_at
    - PostAdmin 관리자 등록:
      * list_display: title, author, views, created_at, updated_at
      * search_fields: title, content
      * list_filter: created_at, updated_at, author
    - apps.users.apps와 apps.posts.apps의 name 속성 수정
    - 데이터베이스 마이그레이션 실행 완료
    
    Issue: #66

--- Blog/apps/posts/models.py
+++ Blog/apps/posts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
+
+class Post(models.Model):
+    """블로그 게시글 모델"""
+    title = models.CharField(max_length=200, verbose_name='제목')
+    content = models.TextField(verbose_name='내용')
+    author = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        verbose_name='작성자',
+        related_name='posts'
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='작성일')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='수정일')
+    views = models.IntegerField(default=0, verbose_name='조회수')
+    
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = '게시글'
+        verbose_name_plural = '게시글 목록'
+    
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'pk': self.pk})
+
